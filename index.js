ab6df322a01e04d27c339664040fc559c87ae21b
anthonypecchillo <anthonypecchillo@gmail.com>
2018-01-07 22:17:11 -0800

    Add printBoard function

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ var startGame = function() {
   console.log('Let the game begin!');
 }
 
+var printBoard = function(board) {
+  console.log(' _ _ _ ');
+  console.log('|' + board[0][0] + '|' + board[0][1] + '|' + board[0][2] + '|');
+  console.log('|— — —|');
+  console.log('|' + board[1][0] + '|' + board[1][1] + '|' + board[1][2] + '|');
+  console.log('|— — —|');
+  console.log('|' + board[2][0] + '|' + board[2][1] + '|' + board[2][2] + '|');
+  console.log(' ¯ ¯ ¯ ');
+}
+
 startGame();
